386985671aff0588dff4d281dfccc1ad92d334d1
MarkMusili <musiliyrn@gmail.com>
2023-12-29 11:36:25 +0300

    Pycodestyle fix

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -9,36 +9,36 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-	"""Function to return a string"""
-	return 'Hello HBNB!'
+    """Function to return a string"""
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-	"""Function to return a string"""
-	return 'HBNB'
+    """Function to return a string"""
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-	"""Function to return a string"""
-	text = escape(text.replace('_', ' '))
-	return f'C {text}'
+    """Function to return a string"""
+    text = escape(text.replace('_', ' '))
+    return f'C {text}'
 
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text='is cool'):
-	"""Function to return a string"""
-	text = escape(text.replace('_', ' '))
-	return f'Python {text}'
+    """Function to return a string"""
+    text = escape(text.replace('_', ' '))
+    return f'Python {text}'
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def is_number(n):
-	"""Function to check for a number"""
-	return f'{n} is a number'
+    """Function to check for a number"""
+    return f'{n} is a number'
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
